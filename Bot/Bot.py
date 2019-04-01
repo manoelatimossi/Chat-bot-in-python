39d30c64ef0cc23adcc5c41eb0971138c48186f2
manoelatimossi <38843787+manoelatimossi@users.noreply.github.com>
2019-04-01 16:48:34 -0300

    Add files via upload

---
+++ Bot/Bot.py
@@ -0,0 +1,17 @@
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+bot = ChatBot ('Avatar',logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'
+    ])
+conversation = ['Hi','Hello', 'How are you?', 'I am fine','How old are you?', '21 years old']
+trainer= ListTrainer(bot)
+trainer.train(conversation)
+while True:
+  question = input("You: ")
+  answer = bot.get_response(question)
+  print('Avatar: ',answer)
+  
+    
+  
